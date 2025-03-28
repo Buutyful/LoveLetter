d0e3559b670776d6be32499dac72eab234f65f8f
Buutyful <mantelli_git@libero.it>
2025-03-28 01:00:17 +0100

    interface impl

--- LoveLetter.Api/Services/GameService.cs
+++ LoveLetter.Api/Services/GameService.cs
@@ -47,6 +47,64 @@ public class GameService(IHubContext<GameHub, IGameHubClient> hub) : IGameServic
         }
         //TODO: handle case user dont respond
     }
+    //TODO: implement other methods and client hooks
+    public Task<ActionParameters> GetPlayerAction(string connId, Card[] available, string[] validTargetsId)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task NotifyTurnStart(Guid id1, string id2)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task NotifyCardPlayed(Guid id1, string id2, Card card, ActionParameters action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyDraw(Guid id1, string id2, IEnumerable<Card> cards)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyDiscard(Guid id1, string id2, Card discarded)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyPlayerEliminated(Guid id1, string id2)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyPrivateInfo(Guid id1, string id2, string v)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyEffectApplied(Guid id1, string id2, Effect effect)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyEffectRemoved(Guid id1, string id2, Effect effect)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyHandUpdate(Guid id1, string id2, IReadOnlyCollection<Card> hand)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyLog(Guid id, string v)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task NotifyGameEnded(Guid id, List<Player> gameWinners)
+    {
+        throw new NotImplementedException();
+    }
 }
